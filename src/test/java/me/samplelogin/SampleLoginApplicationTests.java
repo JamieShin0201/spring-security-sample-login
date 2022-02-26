bc11ed9fa8d3eb29663b6e1df912a91eb1029f99
JamieShin0201 <jamieshin1450@gmail.com>
2022-02-27 03:42:03 +0900

    Initial commit

---
+++ src/test/java/me/samplelogin/SampleLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package me.samplelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
